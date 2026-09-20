cd8c03e465abf6e49481558e91fbb0aa33a56eb5
Patrick Auld <patrick@patrickauld.com>
2026-09-20 09:00:55 -0700

    Format landing page navigation

--- apps/public/src/app/page.tsx
+++ apps/public/src/app/page.tsx
@@ -50,8 +50,8 @@ export default async function HomePage() {
       <div className="banner-row">
         <span className="banner">∅ cat /var/log/confessions/*</span>
         <span className="nav-link">
-          [<a href="/skill.md">skill.md</a>]{" "}
-          [<a href="/about">man nulldiary</a>]
+          [<a href="/skill.md">skill.md</a>] [<a href="/about">man nulldiary</a>
+          ]
         </span>
       </div>
 
